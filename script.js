2c8cb56b8ad676d316a51d2265e4184377db6b44
mathieu <mathieu.morinm@gmail.com>
2026-04-10 13:52:05 +0200

    fonctionement du jeu finit

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-const url = "https://restcountries.com/v3.1/all?fields=name,flags,region,capital,population,cca2";
+const url = "https://restcountries.com/v3.1/all?fields=name,translations,flags,region,capital,population,cca2";
 // Recuperation du bouton et de la zone pour le drapeau
 const button = document.querySelector(".select_country");
 const flag_container = document.querySelector(".show_flag");
+const quizz = document.querySelector(".game");
+const nb_proposition = 3; // Nombre de proposition pour le choix du pays
 
-let countriesCache= [];
+let countriesCache = [];
 // Recuperation de l'API
 async function fetchCountries() {
   const res = await fetch(url);
@@ -11,9 +13,8 @@ async function fetchCountries() {
   const data = await res.json();
 
   return data
-    .filter(c => c?.name?.common && (c?.flags?.svg || c?.flags?.png) && c?.region)
-    .map(c => ({
-      name: c.name.common,
+    .filter(c => c?.name?.common && (c?.flags?.svg || c?.flags?.png) && c?.region).map(c => ({
+      name: c?.translations?.fra?.common || c.name.common,
       flag: c.flags.svg || c.flags.png,
       region: c.region,
       code: c.cca2,
@@ -22,6 +23,20 @@ async function fetchCountries() {
     }));
 }
 
+function getChoices(countries, correctCountry, count) {
+    const choices = [correctCountry];
+
+    while (choices.length < count) {
+        const candidate = getRandomCountry(countries);
+        const alreadyInChoices = choices.some((c) => c.name === candidate.name);
+
+        if (!alreadyInChoices) {
+            choices.push(candidate);
+        }
+    }
+
+    return choices.sort(() => Math.random() - 0.5);
+}
 
 function getRandomCountry(countries) {
     const index = Math.floor(Math.random()*countries.length);
@@ -34,13 +49,57 @@ async function showRandomFlag() {
         }
         const country = getRandomCountry(countriesCache);
         flag_container.innerHTML =`
-            <img src="${country.flag}" alt="Drapeau de ${country.name}" width="400px">
+            <img src="${country.flag}" alt="Drapeau de ${country.name}" width="400px" max-height ="300px" max-width ="400px">
             
-        `
+        `;
+        const choices = getChoices(countriesCache, country, nb_proposition);
+        let quizBox = document.querySelector(".quizz");
+        if (!quizBox) {
+        quizz.insertAdjacentHTML("beforeend", `
+            <div class="quizz">
+            <p>Propositions</p>
+            <div class="choix_pays"></div>
+            <input class="submitBtn" type="button" value="Ma reponse...">
+            <p class="resultat"></p>
+            </div>
+        `);
+        quizBox = document.querySelector(".quizz");
+        }
+
+        const choixPays = quizBox.querySelector(".choix_pays");
+        choixPays.innerHTML = choices.map(c => `
+        <p><input name="bouton_pays" type="radio" value="${c.name}"> ${c.name}</p>
+        `).join("");
+
+        quizBox.dataset.answer = country.name;
+        const result = quizBox.querySelector(".resultat");
+        result.textContent = "";
     } catch (error) {
         console.error(error);
         flag_container.innerHTML = `<p>Impossible de charger les pays.</p>`;
     }
 }
+
+function verify_answer() {
+    const quizBox = document.querySelector(".quizz");
+    if (!quizBox) return;
+
+    const selected = quizBox.querySelector('input[name="bouton_pays"]:checked');
+    const result = quizBox.querySelector(".resultat");
+
+    const bonneReponse = quizBox.dataset.answer;
+
+    if (selected.value === bonneReponse) {
+        flag_container.innerHTML = `<p class = "blink" color = "green">BRAVO !!</p>`
+    } else {
+        flag_container.innerHTML = `<p class = "blink" color = "red">PERDU</p>`
+    }
+}
+
+quizz.addEventListener("click", (event) => {
+    if (event.target.classList.contains("submitBtn")) {
+        verify_answer();
+    }
+});
 
 button.addEventListener("click", showRandomFlag);
